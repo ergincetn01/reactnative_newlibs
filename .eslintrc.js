1a2754ddd8ba4c6ea7bf28bd0e543ff2a5bc5894
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-05-02 16:19:22 +0300

    eslint new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 };
