fd9cb1e68c9600782d262aa7400fd3426003537d
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-05-02 16:22:48 +0300

    home screen with possible navgiatable screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,53 @@
+import {View, Text, Pressable} from 'react-native';
+import React, {FC} from 'react';
+import {IMainStackScreens, MainStackType} from '../navigation/types';
+
+type NavData = {
+  name: keyof MainStackType;
+};
+const NavigationData: NavData[] = [
+  {
+    name: 'FilePicker',
+  },
+  {
+    name: 'ImagePicker',
+  },
+  {
+    name: 'Maps',
+  },
+  {
+    name: 'GestureHandler',
+  },
+  {
+    name: 'DraggableList',
+  },
+  {
+    name: 'Toast',
+  },
+];
+
+type Props = IMainStackScreens<'Home'>;
+const Home: FC<Props> = ({navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', paddingTop: 30}}>
+      <View style={{rowGap: 16, alignItems: 'center'}}>
+        {NavigationData.map((n, ix) => (
+          <Pressable
+            key={ix}
+            style={{
+              borderColor: 'black',
+              borderWidth: 1,
+              paddingHorizontal: 12,
+              paddingVertical: 6,
+              borderRadius: 16,
+            }}
+            onPress={() => navigation.navigate(n.name)}>
+            <Text style={{fontSize: 19}}>{n.name}</Text>
+          </Pressable>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default Home;
